b0279ce710ab8f176e5c862914f22e9f36b4766e
quyetdc <quyetdc.uet@gmail.com>
2019-06-25 17:57:52 +0700

    Installing Bulma + Jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const webpack = require('webpack');
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+);
+
+module.exports = environment;
