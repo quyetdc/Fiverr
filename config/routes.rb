29c40d6d1ac1d01cb6689c7832950a08cff8696d
quyetdc <quyetdc.uet@gmail.com>
2019-07-08 02:16:17 +0700

    Update user without password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
              path: '',
              path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
-             controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+             controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   get 'pages/home'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
