fbecfc9ee77deb65e5d72362d40b1d0e030b2d11
quyetdc <quyetdc.uet@gmail.com>
2019-07-08 10:57:29 +0700

    Add User Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard', to: "users#dashboard" #, as: :dashboard
+  post "users/edit", to: "users#update"
+
   devise_for :users,
              path: '',
              path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
