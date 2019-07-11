768c424dfbd70998085ad10c2bc04003ef39a683
quyetdc <quyetdc.uet@gmail.com>
2019-07-11 14:20:01 +0700

    User add Avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def current_user_params
-    params.require(:user).permit(:from, :about, :status, :language)
+    params.require(:user).permit(:from, :about, :status, :language, :avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
              path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
              controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   get 'pages/home'
-  root to: 'pages#home'
+  root to: "users#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
