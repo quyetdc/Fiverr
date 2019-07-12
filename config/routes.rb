7771da669200535043dac07f0d61cc2c655e3230
quyetdc <quyetdc.uet@gmail.com>
2019-07-12 16:08:18 +0700

    Gig controller default actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'gigs/new'
+  get 'gigs/create'
+  get 'gigs/edit'
+  get 'gigs/update'
+  get 'gigs/show'
   get 'dashboard', to: "users#dashboard" #, as: :dashboard
   get '/users/:id', to: "users#show"
 
@@ -8,7 +13,11 @@ Rails.application.routes.draw do
              path: '',
              path_names: { sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout' },
              controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+
   get 'pages/home'
+
   root to: "users#dashboard"
+  resources :gigs
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
