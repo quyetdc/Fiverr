eb6c6e6ba6d1b2a9eb917c34e06762cfa2f65608
quyetdc <quyetdc.uet@gmail.com>
2019-07-17 11:16:52 +0700

    Edit a gig controller action

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -3,6 +3,7 @@ class GigsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   before_action :set_gig, except: [:new, :create]
   before_action :is_authorized, only: [:edit, :update]
+  before_action :set_step, only: [:update, :edit]
 
   def new
     @gig = current_user.gigs.build
@@ -15,22 +16,80 @@ class GigsController < ApplicationController
     if @gig.save
       #  at this moment, just create 3 different pricings with different pricing_type
       @gig.pricings.create(Pricing.pricing_types.values.map { |x| { pricing_type: x } })
-      redirect_to edit_gig_path(@gig), notice: "Save..."
+      redirect_to edit_gig_path(@gig, step: 2), notice: "Save..."
     else
       redirect_to request.referrer, flash: { error: @gig.errors.full_messages }
     end
   end
 
   def edit
+    @categories = Category.all
   end
 
   def update
+    if @step == 2
+      gig_params[:pricings_attributes].each do |index, pricing|
+        if @gig.has_single_pricing && pricing[:pricing_type] != Pricing.pricing_types.key(0) # Basic pricing type
+          next
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to request.referrer, flash: { error: "Invalid Pricing" }
+          end
+        end
+      end
+    end
+
+    if @step == 3 && gig_params[:description].blank?
+      return redirect_to request.referrer, flash: { error: "Description cannot be blank" }
+    end
+
+    if @step == 4 && @gig.photos.blank?
+      return redirect_to request.referrer, flash: { error: "You don't have any photos" }
+    end
+
+    if @step == 5
+
+      @gig.pricings.each do |pricing|
+        if @gig.has_single_pricing && !pricing.basic?
+          next
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to edit_gig_path(@gig, step: 2), flash: { error: "Invalid pricing" }
+          end
+        end
+      end
+
+      if @gig.description.blank?
+        return redirect_to edit_gig_path(@gig, step: 3), flash: { error: "Description cannot be blank" }
+      elsif @gig.photos.blank?
+        return redirect_to edit_gig_path(@gig, step: 4), flash: { error: "You don't have any photos" }
+      end
+    end
+
+    if @gig.update(gig_params)
+      flash[:notice] = "Saved..."
+    else
+      return redirect_to request.referrer, flash: { error: @gig.errors.full_messages }
+    end
+
+    if @step < 5
+      redirect_to edit_gig_path(@gig, step: @step + 1)
+    else
+      redirect_to dashboard_path
+    end
   end
 
   def show
   end
 
   private
+
+  def set_step
+    @step = params[:step].to_i > 0 ? params[:step].to_i : 1
+    if @step > 5
+      @step = 5
+    end
+  end
 
   def set_gig
     @gig = Gig.find(params[:id])
@@ -43,8 +102,8 @@ class GigsController < ApplicationController
 
   def gig_params
     params.require(:gig).permit(
-      :title, :video, :active, :category_id, :has_single_pricing,
-      pricing_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type]
+      :title,:description, :video, :active, :category_id, :has_single_pricing,
+      pricings_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type]
     )
   end
 end
