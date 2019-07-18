b386944d9acbb8a2ef198a789702b7fd1bb4a2b8
quyetdc <quyetdc.uet@gmail.com>
2019-07-18 16:49:13 +0700

    Update user dashboard

--- app/helpers/gigs_helper.rb
+++ app/helpers/gigs_helper.rb
@@ -1,2 +1,13 @@
 module GigsHelper
+  def gig_cover gig
+    if gig.photos.attached?
+      url_for(gig.photos[0])
+    else
+      ActionController::Base.helpers.asset_path('icon_default_image.png')
+    end
+  end
+
+  def basic_pricing gig
+    gig.pricings.find { |p| p.pricing_type == "basic" }
+  end
 end
--- app/javascript/packs/users.js
+++ app/javascript/packs/users.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).on('turbolinks: load', () => {
   $('.toggle').click((e) => {
     e.stopPropagation();
     e.preventDefault();
