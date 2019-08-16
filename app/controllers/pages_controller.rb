2a9f9108144db3abcfc4980eb50fb0662cffc674
quyetdc <quyetdc.uet@gmail.com>
2019-08-16 17:07:03 +0700

    Search Gig by multiple conditions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,10 @@ class PagesController < ApplicationController
     query_condition[0] = "gigs.active = true"
 
     @q = params[:q]
+    @min = params[:min]
+    @max = params[:max]
+    @delivery = params[:delivery].present? ? params[:delivery] : '0'
+
     unless @q.blank?
       query_condition[0] += ' AND gigs.title ILIKE ?'
       query_condition.push "%#{@q}%"
@@ -22,7 +26,25 @@ class PagesController < ApplicationController
       query_condition.push params[:category]
     end
 
-    @gigs = Gig.where(query_condition)
+    unless params[:min].blank?
+      query_condition[0] += ' AND pricings.price >= ?'
+      query_condition.push @min
+    end
+
+    unless params[:max].blank?
+      query_condition[0] += ' AND pricings.price <= ?'
+      query_condition.push @max
+    end
+
+    if !params[:delivery].blank? && params[:delivery] != "0"
+      query_condition[0] += ' AND pricings.delivery_time <= ?'
+      query_condition.push @delivery
+    end
+
+    @gigs = Gig
+              .select('gigs.id, gigs.title, gigs.user_id, pricings.price AS price')
+              .joins(:pricings)
+              .where(query_condition)
   end
 
 end
