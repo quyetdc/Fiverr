8b8613384c3dfecf987b5bacabc3bdb3dc13a866
quyetdc <quyetdc.uet@gmail.com>
2019-08-16 15:07:51 +0700

    Search by multiple conditions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,24 @@ class PagesController < ApplicationController
   def search
     @categories = Category.all.order(name: :asc)
     @category = Category.find(params[:category]) if params[:category].present?
+
+    # @gigs = Gig.where("active = ? AND gigs.title ILIKE ? AND category_id = ?", true, "%#{params[:q]}%", params[:category])
+
+    query_condition = []
+    query_condition[0] = "gigs.active = true"
+
+    @q = params[:q]
+    unless @q.blank?
+      query_condition[0] += ' AND gigs.title ILIKE ?'
+      query_condition.push "%#{@q}%"
+    end
+
+    unless params[:category].blank?
+      query_condition[0] += ' AND category_id = ?'
+      query_condition.push params[:category]
+    end
+
+    @gigs = Gig.where(query_condition)
   end
 
 end
